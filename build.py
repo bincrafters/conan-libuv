922402f0477d77e71a633d49bb47cfa9520ed833
Jerry W <jerrywiltse@gmail.com>
2018-01-22 11:45:15 -0500

    fix add build_requires for all builds

--- build.py
+++ build.py
@@ -7,13 +7,10 @@ from bincrafters import build_shared
 import os
 
 def add_build_requires(builds):
-    if os.getenv('MINGW_CONFIGURATIONS', ''):
-        return map(add_required_installers, builds)
-    else:
-        return builds
+    return map(add_required_installers, builds)
 
 def add_required_installers(build):
-    installers = ['ninja_installer/1.8.2@bincrafters/testing', 'gyp_installer/20171101@bincrafters/testing']
+    installers = ['ninja_installer/1.8.2@bincrafters/stable', 'gyp_installer/20171101@bincrafters/stable']
     build.build_requires.update({"*" : installers})
     return build
 
