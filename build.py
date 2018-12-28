dc743433ad7fa2b269fa463f9b4d87b4bbab4299
Uilian Ries <uilianries@gmail.com>
2018-12-28 15:41:13 -0200

    Update build script

--- build.py
+++ build.py
@@ -3,22 +3,10 @@
 
 
 from bincrafters import build_template_default
-from bincrafters import build_shared
 import os
 
-def add_build_requires(builds):
-    return map(add_required_installers, builds)
-
-def add_required_installers(build):
-    installers = ['ninja_installer/1.8.2@bincrafters/stable', 'gyp_installer/20171101@bincrafters/stable']
-    build.build_requires.update({"*" : installers})
-    return build
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
-    builder.items = add_build_requires(builder.items)
-    
     builder.run()
-    
